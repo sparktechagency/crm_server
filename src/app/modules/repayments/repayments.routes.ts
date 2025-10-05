d4abf07e31b52152dfa3ce7e3a1222c9e903ae10
developer-mahin <mdmahin1310@gmail.com>
2025-10-05 19:50:51 +0600

    refactor: leads and clients and repayments service to grand permission spokemanager

--- src/app/modules/repayments/repayments.routes.ts
+++ src/app/modules/repayments/repayments.routes.ts
@@ -16,7 +16,7 @@ router
   )
   .get(
     '/',
-    auth(USER_ROLE.fieldOfficer, USER_ROLE.admin, USER_ROLE.hubManager),
+    auth(USER_ROLE.fieldOfficer, USER_ROLE.admin, USER_ROLE.hubManager, USER_ROLE.spokeManager),
     RepaymentsController.getAllRepayments,
   )
   .patch(
